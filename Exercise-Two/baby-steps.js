210ff708dd4fd9e307915d0c68964b18abd02376
surabhi8 <surabhigupta2442@gmail.com>
2018-01-28 19:39:34 +0530

    Second exercise done and verified

--- Exercise-Two/baby-steps.js
+++ Exercise-Two/baby-steps.js
@@ -1,6 +1,8 @@
-function addCommandLineArguments(argumentsToAdd) {
-  // const arguments = process.argv.slice(2);
+function addCommandLineArguments(/*argumentsToAdd*/) {
+   const arguments = process.argv.slice(2);
   const finalValue = argumentsToAdd.map(argument => Number(argument)).reduce((total, number) => total + number);
   global.console.log(finalValue);
 }
+//Comment below line to test and pass argument to function
+addCommandLineArguments();
 module.exports = addCommandLineArguments;
